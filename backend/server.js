893a5d4107535590cb366b4b7ab165d3c902e698
Abhijit Aundhkar <abhijitaundhkar@gmail.com>
2024-03-11 22:55:17 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", userRoutes);
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+//connect files with front-end
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
